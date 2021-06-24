6a978f3335e7a2b1be9a2b470367b8b8f4921f53
jezzbox <keeltyj@protonmail.com>
2021-06-24 18:12:07 +0100

    fixed write csv

--- awsutils/s3.py
+++ awsutils/s3.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def read_s3_file(bucket, client, key, aws_details,file_type="csv"):
+def read_s3_file(bucket, client, key, aws_details,file_type="csv",dtype=None):
 
     file_location = f"s3://{bucket}/{client}/{key}"
     storage_options={
@@ -9,18 +9,18 @@ def read_s3_file(bucket, client, key, aws_details,file_type="csv"):
                 }
     if file_type == "csv":
         df = pd.read_csv(file_location,
-            storage_options=storage_options,
+            storage_options=storage_options,dtype=dtype,
             )
         return df
 
     elif file_type == "xlsx" or file_type == "xls":
-        df = pd.read_excel(file_location,engine="openpyxl",
+        df = pd.read_excel(file_location,engine="openpyxl",dtype=dtype,
             storage_options=storage_options,
             )
         return df
 
 
-def write_s3_file(bucket, client, key, aws_details):
+def write_s3_file(df,bucket, client, key, aws_details):
 
     file_location = f"s3://{bucket}/{client}/{key}"
     storage_options={
@@ -28,7 +28,7 @@ def write_s3_file(bucket, client, key, aws_details):
                 "secret": aws_details["secret_access_key_id"],
                 }
     
-    pd.to_csv(file_location,
+    df.to_csv(file_location,
             index=False,
             storage_options=storage_options,
             )
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Needed for dependencies
     install_requires=['pandas','s3fs','openpyxl'],
     # *strongly* suggested for sharing
-    version='0.1',
+    version='0.2',
     # The license can be anything you like
     license='MIT',
     description='some aws functions for ease of use',
